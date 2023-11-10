1957c4ecee50613f1952dda226e56a65ab8fd204
Seunggyo <tmdry99@gmail.com>
2023-11-10 10:41:23 +0900

    게시물 수정하기 기능 완성

--- src/main/java/com/example/prj1be/controller/BoardController.java
+++ src/main/java/com/example/prj1be/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +51,11 @@ public class BoardController {
         } else {
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    @PutMapping("edit")
+    public void edit(@RequestBody Board board) {
+//        System.out.println("board = " + board);
+        service.update(board);
     }
 }
